94aca3dde2a7023ae7963a278da16f7e1952f721
aashisrjl <aashisrijal252@gmail.com>
2026-09-13 12:31:32 +0545

    update: chatbot add

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -116,6 +116,22 @@ function RootShell({ children }: { children: ReactNode }) {
       <body>
         {children}
         <Scripts />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+              (function () {
+                var s1 = document.createElement("script");
+                var s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = "https://embed.tawk.to/6aa645849f6293344272202c/1k2cnv3os";
+                s1.charset = "UTF-8";
+                s1.setAttribute("crossorigin", "*");
+                s0.parentNode.insertBefore(s1, s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
